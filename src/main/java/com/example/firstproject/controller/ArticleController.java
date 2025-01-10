cfe4033d45eaead4778a8c40ee5820073acde4f1
tdj4909 <tdj4909@gmail.com>
2025-01-10 21:28:53 +0900

    update

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -25,11 +29,32 @@ public class ArticleController {
         //System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        //System.out.println(article.toString());
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         //System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //3. 뷰페이지 반환하기
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
